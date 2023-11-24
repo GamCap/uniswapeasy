1082b3b23ef180937f2dbc9e1172f8ebc8685f67
hberkaykuran <hberkaykuran@outlook.com>
2023-11-24 13:44:59 +0300

    fixed alias issues, configured babel/tsconfig/rollup, examples folder draft

---
+++ examples/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { UniswapEasy } from "uniswapeasy";
+const App = () => {
+  return (
+    <div>
+      <h1>Widget Example</h1>
+      <UniswapEasy theme={{}} />
+    </div>
+  );
+};
+
+export default App;
